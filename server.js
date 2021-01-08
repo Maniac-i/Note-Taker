f659c9cac2ae92c71e72589e8f7a286041bcc6af
Joe Maniaci <joe.maniaci1@gmail.com>
2021-01-08 15:28:29 -0500

    created the post api route

--- server.js
+++ server.js
@@ -10,22 +10,29 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
+//Reads db.json file and returns contents as JSON
 app.get("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", "utf8", (err, state) => {
-    if (err) throw err;
-    
-    console.log(state);
-    res.json((state));
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
 
+    if (err) throw err;
+
+    res.json((data));
   })
 });
 
+//Recieves new note and adds it to the db.json file and returns the notes to the client
 app.post("/api/notes", (req, res) => {
-  
+  let newNote = JSON.stringify(req.body);
+  fs.appendFile("./db/db.json", newNote, (err) => {
+    if (err) throw err;
+
+    res.json(newNote);
+  })
+
 });
 
 app.delete("/api/notes/:id", (req, res) => {
-  
+
 });
 
 app.listen(PORT, () => {
